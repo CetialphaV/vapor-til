95db1e891d99dbbfc0e9f5fbca3cb4f3040af480
Logan Rivera <loganrivera@Logans-Macbook-Pro.local>
2018-08-22 16:17:14 -0500

    Use PostgreSQL as the database

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -10,7 +10,6 @@ public func routes(_ router: Router) throws {
     router.post("api", "acronyms") { req -> Future<Acronym> in
         return try req.content.decode(Acronym.self).flatMap(to: Acronym.self) { acronym in
             return acronym.save(on: req)
-            
         }
     }
 
